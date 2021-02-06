e76968c60aba4fefbade0b83b334e0e3e98c626c
Waqar <Waqar@Waqar>
2021-02-06 13:23:12 +0530

    WH-805-First Time Update

---
+++ src/main/java/com/waqar/GitPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.waqar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPracticeApplication.class, args);
+		System.out.println("Git-Practice");
+	}
+
+}
