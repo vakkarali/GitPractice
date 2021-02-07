7b9c7189fa04d8494d0ae42ed9c3dc412ad1068b
Waqar <Waqar@Waqar>
2021-02-07 12:56:02 +0530

    New File

---
+++ src/main/java/com/waqar/runner/MessageRunner.java
@@ -0,0 +1,14 @@
+package com.waqar.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("Welcome to Git-Hub");
+	}
+}
